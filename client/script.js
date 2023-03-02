c002eb4348c441038963d8ce23fc740a89f377b5
Ryan Barley <rmbarley@gmail.com>
2023-03-01 21:13:13 -0500

    Point api call to hosted server

--- client/script.js
+++ client/script.js
@@ -63,7 +63,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://rmbarley-codex.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
